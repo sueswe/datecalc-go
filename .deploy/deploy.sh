3f8500245b7ba59f4aa43807647d565bad7cf250
Werner Süß <werner.suess@itsv.at>
2025-05-09 08:50:21 +0200

    Added: deploy-scripte

---
+++ .deploy/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source ~/.profile
+
+cd "$HOME"/compile/datecalc-go || {
+    echo "Status: $?"
+    exit 4
+}
+
+echo "------------------------------------"
+env | grep PATH
+env | grep LOADED
+echo "------------------------------------"
+
+
+
+echo '
+### LINUX #########################################################
+'
+
+stages="
+stp,testta3
+lgkk,testta3
+"
+
+for UMG in ${stages}
+do
+    cd /tmp/ || exit 1
+    "$HOME"/bin/vicecersa.sh "${UMG}" datecalc \$HOME/bin/ || {
+        echo "Status: $?"
+        exit 2
+    }
+done
+
+
+echo '
+### AIX #########################################################
+'
