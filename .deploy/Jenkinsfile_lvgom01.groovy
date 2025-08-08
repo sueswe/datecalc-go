91de56a5a2be6094dba95dc4d652c55f65e8c517
Werner Süß <werner.suess@itsv.at>
2025-08-08 11:42:01 +0200

    Jenkinsfile_lvgom01.groovy: diff --git a/.deploy/Jenkinsfile_lvgom01.groovy b/.deploy/Jenkinsfile_lvgom01.groovy index ba1f84c..c9a40b3 100644 --- a/.deploy/Jenkinsfile_lvgom01.groovy +++ b/.deploy/Jenkinsfile_lvgom01.groovy @@ -42,7 +42,7 @@ pipeline {                      try {                          sh ' cd ~/compile/datecalc-go && .deploy/deploy.sh '                      } catch(err) { -                        throw err +                        unstable "${err}"                      }                  }              }

--- .deploy/Jenkinsfile_lvgom01.groovy
+++ .deploy/Jenkinsfile_lvgom01.groovy
@@ -42,7 +42,7 @@ pipeline {
                     try {
                         sh ' cd ~/compile/datecalc-go && .deploy/deploy.sh '
                     } catch(err) {
-                        throw err
+                        unstable "${err}"
                     }
                 }
             }
